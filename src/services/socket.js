5a22c849b11da18c8a2cd0d3f1369546864f4a5d
ajayVtalix <kumar.ajay@vtalix.in>
2025-12-29 14:27:41 +0530

    backend url updated

--- src/services/socket.js
+++ src/services/socket.js
@@ -8,6 +8,6 @@
 
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:3044", {
+export const socket = io("https://back-signal.onrender.com/", {
   autoConnect: false
 });
